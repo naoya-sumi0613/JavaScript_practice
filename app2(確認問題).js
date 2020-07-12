c0079804fd789b99f88f841187dc4c77eb39f0f0
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-07-12 20:21:32 +0900

    Add 確認問題

---
+++ app2(確認問題).js
@@ -0,0 +1,9 @@
+var min = 0;
+var max = 10;
+var num1 = Math.floor(Math.random() * (max + 1 - min)) + min ;
+var num2 = Math.floor(Math.random() * (max + 1 - min)) + min ;
+
+alert(num1 + num2);
+alert(num1 - num2);
+alert(num1 * num2);
+alert(num1 / num2);
